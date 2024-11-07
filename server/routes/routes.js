9e8b20048f87d45e7ac8cbc64943cfc83d7d9d27
akhilkailas017 <akhilkailas2001@gmail.com>
2024-11-07 20:19:26 +0530

    add non pdc function to routes.js and test with postman

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -68,173 +68,197 @@ router.post("/createWaste", async (req, res) => {
 });
 
 
+router.post("/deleteWaste", async (req, res) => {
+  try {
+    const { wasteId } = req.body;
+
+    let WasteCollectionCompanyClient = new clientApplication();
+
+    const result = await WasteCollectionCompanyClient.submitTxn(
+      "WasteCollectionCompany",
+      "managementchannel",
+      "basic",
+      "collectionContract",
+      "invokeTxn",
+      "",
+      "deleteWaste",
+      wasteId
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "waste deleted successfully!",
+      data: { result },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Please check the  ID!",
+      data: { error },
+    });
+  }
+});
+
+router.post("/updateWasteDetails", async (req, res) => {
+  try {
+    const { wasteId , reusableWeight, owner } = req.body;
+
+    let recyclingCenterClient = new clientApplication();
+
+    const result = await recyclingCenterClient.submitTxn(
+      "recyclingCenter",
+      "managementchannel",
+      "basic",
+      "collectionContract",
+      "invokeTxn",
+      "",
+      "updateWasteDetails",
+      wasteId,
+      reusableWeight,
+      owner
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "waste updated successfully!",
+      data: { result },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Please check the  ID!",
+      data: { error },
+    });
+  }
+});
+
+router.post("/buyWaste", async (req, res) => {
+  try {
+    const { wasteId , owner } = req.body;
+
+    let manufacturerClient = new clientApplication();
+
+    const result = await manufacturerClient.submitTxn(
+      "manufacturer",
+      "managementchannel",
+      "basic",
+      "collectionContract",
+      "invokeTxn",
+      "",
+      "buyWaste",
+      wasteId,
+      owner
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "waste buyed successfully!",
+      data: { result },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Please check the  ID!",
+      data: { error },
+    });
+  }
+});
+
+
+router.post("/createProduct", async (req, res) => {
+  try {
+    const { productId, wasteId , name } = req.body;
+
+    let manufacturerClient = new clientApplication();
+
+    const result = await manufacturerClient.submitTxn(
+      "manufacturer",
+      "managementchannel",
+      "basic",
+      "collectionContract",
+      "invokeTxn",
+      "",
+      "createProduct",
+      productId,
+      wasteId,
+      name
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully!",
+      data: { result },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Please check the  ID!",
+      data: { error },
+    });
+  }
+});
+
+router.post("/readProduct", async (req, res) => {
+  try {
+    const { productId } = req.body;
+    let manufacturerClient = new clientApplication();
+    let waste = await manufacturerClient.submitTxn(
+      "manufacturer",
+      "managementchannel",
+      "basic",
+      "collectionContract",
+      "queryTxn",
+      "",
+      "readProduct",
+      productId
+    );
+    const data = new TextDecoder().decode(waste);
+    const value = JSON.parse(data);
+
+    res.status(200).json({
+      success: true,
+      message: "product read successfully!",
+      data: { value },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Please check the ID!",
+      data: { error },
+    });
+  }
+});
+
+router.post("/deleteProduct", async (req, res) => {
+  try {
+    const { productId } = req.body;
+
+    let manufacturerClient = new clientApplication();
+
+    const result = await manufacturerClient.submitTxn(
+      "manufacturer",
+      "managementchannel",
+      "basic",
+      "collectionContract",
+      "invokeTxn",
+      "",
+      "deleteProduct",
+      productId,
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Product deleted successfully!",
+      data: { result },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Please check the  ID!",
+      data: { error },
+    });
+  }
+});
+
+
 module.exports = router;
-
-
-// const express = require("express");
-// const router = express.Router();
-// const { clientApplication } = require("./client");
-
-// router.post("/createWaste", async (req, res) => {
-//   try {
-//     const { wasteId, collectionCompany, totalWeight, owner } = req.body;
-//     const mspID = "WasteCollectionCompanyMSP";  // Only WasteCollectionCompanyMSP can create waste
-
-//     let userClient = new clientApplication();
-//     const result = await userClient.submitTxn(
-//       mspID,
-//       "managementchannel",
-//       "basic",
-//       "collectionContract",
-//       "invokeTxn",
-//       "",
-//       "createWaste",
-//       wasteId,
-//       collectionCompany,
-//       totalWeight,
-//       owner
-//     );
-
-//     res.status(201).json({
-//       success: true,
-//       message: "Waste record created successfully!",
-//       data: JSON.parse(result.toString())
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: "Failed to create waste record. Please check the input data!",
-//       data: { error: error.message },
-//     });
-//   }
-// });
-
-// router.post("/readWaste", async (req, res) => {
-//   try {
-//     const { wasteId } = req.body;
-
-//     let userClient = new clientApplication();
-//     const wasteRecord = await userClient.submitTxn(
-//       "WasteCollectionCompany",
-//       "managementchannel",
-//       "basic",
-//       "collectionContract",
-//       "queryTxn",
-//       "",
-//       "readWaste",
-//       wasteId
-//     );
-
-//     const wasteData = JSON.parse(wasteRecord.toString());
-//     res.status(200).json({
-//       success: true,
-//       message: "Waste data retrieved successfully!",
-//       data: wasteData,
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: "Failed to read waste data. Please check the Waste ID!",
-//       data: { error: error.message },
-//     });
-//   }
-// });
-
-// router.post("/updateWasteDetails", async (req, res) => {
-//   try {
-//     const { wasteId, reusableWeight, owner } = req.body;
-//     const mspID = "recyclingCenterMSP"; // Only recyclingCenterMSP can update waste details
-
-//     let userClient = new clientApplication();
-//     const result = await userClient.submitTxn(
-//       mspID,
-//       "managementchannel",
-//       "basic",
-//       "collectionContract",
-//       "invokeTxn",
-//       "",
-//       "updateWasteDetails",
-//       wasteId,
-//       reusableWeight,
-//       owner
-//     );
-
-//     res.status(200).json({
-//       success: true,
-//       message: `Waste details for ID ${wasteId} updated successfully.`,
-//       data: JSON.parse(result.toString())
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: "Failed to update waste details.",
-//       data: { error: error.message },
-//     });
-//   }
-// });
-
-// router.post("/buyWaste", async (req, res) => {
-//   try {
-//     const { wasteId, owner } = req.body;
-//     const mspID = "manufacturerMSP"; // Only manufacturerMSP can buy waste
-
-//     let userClient = new clientApplication();
-//     const result = await userClient.submitTxn(
-//       mspID,
-//       "managementchannel",
-//       "basic",
-//       "collectionContract",
-//       "invokeTxn",
-//       "",
-//       "buyWaste",
-//       wasteId,
-//       owner
-//     );
-
-//     res.status(200).json({
-//       success: true,
-//       message: `Waste with ID ${wasteId} bought successfully.`,
-//       data: JSON.parse(result.toString())
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: "Failed to buy waste. Please check the input data!",
-//       data: { error: error.message },
-//     });
-//   }
-// });
-
-// router.post("/useVoucher", async (req, res) => {
-//   try {
-//     const { wasteId, voucherId } = req.body;
-//     const mspID = "governmentMSP"; // Only governmentMSP can use vouchers
-
-//     let userClient = new clientApplication();
-//     const result = await userClient.submitTxn(
-//       mspID,
-//       "managementchannel",
-//       "basic",
-//       "collectionContract",
-//       "invokeTxn",
-//       "",
-//       "useVoucher1",
-//       wasteId,
-//       voucherId
-//     );
-
-//     res.status(200).json({
-//       success: true,
-//       message: `Voucher with ID ${voucherId} used successfully for waste ID ${wasteId}.`,
-//       data: JSON.parse(result.toString())
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: "Failed to use voucher. Please check the input data!",
-//       data: { error: error.message },
-//     });
-//   }
-// });
-
-// module.exports = router;
